2c83e88925711b633a829a24b6d8eaee4b54cf0b
ingridmartsanchez1996 <ingridmartsanchez4@gmail.com>
2023-10-16 18:47:44 -0600

    adding changes

--- formulario.js
+++ formulario.js
@@ -1,5 +1,7 @@
 // In all the lines it was missed ; at the end of each line.
-var formulario = document.querySelector("#form");
+
+//Changing the name to formulario and changing # to . because it's a class in the HTML code
+var formulario = document.querySelector(".formulario");
 
 formulario.onsubmit = function(e) {
     // Changing from "prevent" to "preventDefault"
@@ -9,9 +11,10 @@ formulario.onsubmit = function(e) {
     // and reload the webpage.
   e.preventDefault();
   
-  var n = formulario.elements[0];
-  var e = formulario.elements[1];
-  var na = formulario.elements[2];
+  //Changing names of variables (n -> nombre) (e -> edad) (n -> nacionalidad)
+  var nombre = formulario.elements[0];
+  var edad = formulario.elements[1];
+  var nacionalidad = formulario.elements[2];
 
   var nombre = n.value;
   var edad = e.value;
@@ -21,11 +24,12 @@ formulario.onsubmit = function(e) {
   console.log(nombre, edad);
   console.log(nacionalidad);
 
+  //Validating inputs
   if (nombre.length === 0) {
-    n.classList.add("error");
+    nombreInput.classList.add("error");
   }
   if (edad < 18 || edad > 120) {
-    e.classList.add("error");
+    edadInput.classList.add("error");
   }
 
   if (nombre.length > 0 && (edad > 18 && edad < 120)) {
